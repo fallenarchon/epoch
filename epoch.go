c35a73c54d031e83d2918d735f5b796d8e7985c2
fallenarchon <joeamrhein@gmail.com>
2016-07-09 21:08:52 -0700

    Parse input from stdin

--- epoch.go
+++ epoch.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
 	"strconv"
 	"time"
 )
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	printHumanReadable := flag.Bool("h", false, "Prints human readable date")
@@ -14,12 +22,14 @@ func main() {
 
 	t := time.Now()
 
-	if len(flag.Args()) == 1 {
-		millis, err := strconv.ParseInt(flag.Arg(0), 10, 64)
-		if err != nil {
-			panic(err)
-		}
-		t = time.Unix(0, millis*1000000)
+	info, err := os.Stdin.Stat()
+	checkErr(err)
+
+	//If data is being piped in, parse millis from that
+	if info.Size() > 0 {
+		t = convertStringMillisToTime(fetchInputFromPipe())
+	} else if len(flag.Args()) == 1 {
+		t = convertStringMillisToTime(flag.Arg(0))
 	}
 
 	fmt.Printf("Epoch millis:\t%v\n", t.UnixNano()/1000000)
@@ -30,6 +40,19 @@ func main() {
 		fmt.Printf("Local:\t%v\n", formatTime(&t))
 	}
 
+}
+
+//Currently only fetches the first token in the pipe.  Future work could spit out data for each token
+func fetchInputFromPipe() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func convertStringMillisToTime(inputMillis string) time.Time {
+	millis, err := strconv.ParseInt(inputMillis, 10, 64)
+	checkErr(err)
+	return time.Unix(0, millis*1000000)
 }
 
 func formatTime(t *time.Time) string {
